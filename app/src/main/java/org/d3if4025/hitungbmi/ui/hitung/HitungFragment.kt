2df8861a249130b89c62bd6a52177ce3fed9d08e
Rachmad_Sukri <rachmadsukri@student.telkomuniversity.ac.id>
2021-04-27 21:52:40 +0700

    Menyimpan data ke database

--- app/src/main/java/org/d3if4025/hitungbmi/ui/hitung/HitungFragment.kt
+++ app/src/main/java/org/d3if4025/hitungbmi/ui/hitung/HitungFragment.kt
@@ -3,21 +3,29 @@ package org.d3if4025.hitungbmi.ui.hitung
 import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
 import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.observe
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import org.d3if4025.hitungbmi.R
 import org.d3if4025.hitungbmi.data.KategoriBmi
 import org.d3if4025.hitungbmi.databinding.FragmentHitungBinding
+import org.d3if4025.hitungbmi.db.BmiDb
 import org.d3if4025.hitungbmi.ui.hitung.HitungFragmentDirections
 
 class HitungFragment : Fragment() {
 
-    private val viewModel: HitungViewModel by viewModels()
+    private val viewModel: HitungViewModel by lazy {
+        val db = BmiDb.getInstance(requireContext())
+        val factory = HitungViewModelFactory(db.dao)
+        ViewModelProvider(this, factory).get(HitungViewModel::class.java)
+    }
     private lateinit var binding: FragmentHitungBinding
     private var bmi = 1f
     private var status =  ""
@@ -47,6 +55,11 @@ class HitungFragment : Fragment() {
             binding.kategoriTextView.text = getString(R.string.kategori_x, getKategori(it.kategori))
             binding.saranButton.visibility = View.VISIBLE
             binding.shareButton.visibility = View.VISIBLE
+        })
+
+        viewModel.data.observe(viewLifecycleOwner, {
+            if (it == null) return@observe
+            Log.d("HitungFragment", "Data tersimpan. ID = ${it.id}")
         })
     }
 
